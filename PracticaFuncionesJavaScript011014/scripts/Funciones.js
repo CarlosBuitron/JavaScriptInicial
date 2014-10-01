ad52a6abcff8c0fa452b207482827e9b83ac50e7
CarlosBuitron <carlos.buitron@tajamar365.com>
2014-10-01 13:37:17 +0200

    Primer ejercicio Javascript

---
+++ PracticaFuncionesJavaScript011014/scripts/Funciones.js
@@ -0,0 +1,36 @@
+﻿var arrValores = new  Array();
+
+(function () {
+    var number = 0;
+    do {
+        number = prompt("Dime un número");
+        if (!isNaN(number)) {
+            arrValores.push(number);
+        }
+    } while (!isNaN(number));
+})();
+
+function sumar() {
+    var result = 0;
+    for (var i = 0; i < arrValores.length; i++) {
+        result = result + parseInt(arrValores[i]);
+    }
+    return result;
+}
+
+function media() {
+    this.rmedia = sumar() / arrValores.length;
+    
+}
+
+var obj = { rmedia: 0 };
+
+function escribir()
+{
+    alert("La suma total es: " + sumar());
+    media.call(obj);
+    alert("La media total es: " + obj.rmedia);
+
+}
+
+escribir();
